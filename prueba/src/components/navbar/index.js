1c5159323eefb3d2ef5835dee147ccb3275c6dad
Spoma89 <sebastianromeropoma89@gmail.com>
2022-02-21 12:07:38 -0300

    navegador

---
+++ prueba/src/components/navbar/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+
+
+const Navbar = props => (
+  
+
+<ul className="nav justify-content-center">
+  <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="Productos">Productos</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Ofertas</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Quienes somos</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Cotizaciones</a>
+  </li>
+  
+</ul>
+
+  
+);
+
+export default Navbar;
