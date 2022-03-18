082105e5bfc243c338e24f65e65125960dae6d66
Spoma89 <sebastianromeropoma89@gmail.com>
2022-03-17 23:48:38 -0300

    sincronizarcounter

--- prueba/src/components/itemdetail/ItemDetail.js
+++ prueba/src/components/itemdetail/ItemDetail.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ItemCount from '../../components/contador/ItemCount.js'
+
+
+
 
 const ItemDetail = ({producto})=>{
-
+  const [count, setCount] = useState(null)
 	return(
 		<div>
 		<h2 className="deta">{producto.name}</h2>
 		<h3>${producto.price}</h3>
 		<img src={producto.foto} className="r" alt="cemento"></img>
 		<p>{producto.categoria}</p>
+		<ItemCount initial = {1} stock = {10} onAdd={ onAdd } />
 	</div>
 	)
 }
